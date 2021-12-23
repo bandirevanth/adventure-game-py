8f25751fd84e7ada762903dcfb54a6751359e9e9
Bandi Revanth <dhanush24082014@gmail.com>
2021-12-23 14:46:55 +0530

    Add files via upload

---
+++ adventure.py
@@ -0,0 +1,42 @@
+print("Get started by typing your name.")
+name = input("Type your name: ")
+print("Welcome", name, "to this adventure!")
+
+answer = input(
+    "You are on a dirt road, it has come to an end and you can go left or right. Which way would you like to go? ").lower()
+
+if answer == "left":
+    answer = input(
+        "You come to a river, you can walk around it or swim accross? Type walk to walk around and swim to swim accross: ")
+
+    if answer == "swim":
+        print("You swam acrross and WON!")
+    elif answer == "walk":
+        print("You walked for many miles, ran out of water and you lost the game.")
+    else:
+        print('Not a valid option. You lose.')
+
+elif answer == "right":
+    answer = input(
+        "You come to a bridge, it looks wobbly, do you want to cross it or head back (cross/back)? ")
+
+    if answer == "back":
+        print("You go back and lose.")
+    elif answer == "cross":
+        answer = input(
+            "You cross the bridge and meet a stranger. Do you talk to them (yes/no)? ")
+
+        if answer == "yes":
+            print("You LOSE!")
+        elif answer == "no":
+            print("Good Job! Never speak to people without getting to know them.")
+        else:
+            print('Not a valid option. You lose.')
+    else:
+        print('Not a valid option. You lose.')
+
+else:
+    print('Not a valid option. You lose.')
+
+print("Thank you for trying", name)
+print("Game Created By Bandi Revanth.")
